7574572870068c8b0283380c427a39816f0327d0
Gustavo Baranda <baranda.gustavo@gmail.com>
2025-05-20 21:09:18 -0300

    restoques finales

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import './Sidebar.css';
+import Logo  from '../../assets/BreakingCode.png';
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,10 +12,23 @@ const Sidebar = () => {
   return (
     <aside className={`sidebar ${isOpen ? 'open' : ''}`}>
       <button className='menu-toggle' onClick={toggleMenu}>
-        <span className='hamburger'></span>
+        {isOpen ? (
+          // Ícono de "X"
+          <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line x1="6" y1="6" x2="22" y2="22" stroke="#fff" strokeWidth="3" strokeLinecap="round"/>
+            <line x1="22" y1="6" x2="6" y2="22" stroke="#fff" strokeWidth="3" strokeLinecap="round"/>
+          </svg>
+        ) : (
+          // Ícono de menú
+          <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect y="5" width="28" height="3" rx="1.5" fill="#fff"/>
+            <rect y="12.5" width="28" height="3" rx="1.5" fill="#fff"/>
+            <rect y="20" width="28" height="3" rx="1.5" fill="#fff"/>
+          </svg>
+        )}
       </button>
       <div className='logo-container'>
-        <img src='https://scontent.fepa9-2.fna.fbcdn.net/v/t39.30808-6/358102715_958697115394429_2463043128421865178_n.png?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeEAWnQFj5tAiAfUYZwQoO58mxY6_66vpxGbFjr_rq-nEczwez-udI-p-kBTmhZwpocBYBHBQ6e_R-S8htJFI5sb&_nc_ohc=exIvmaD0h2UQ7kNvwF2SSkc&_nc_oc=Adn-wJaO_G1uKWzwB4NrcqEnkZnsVVpQ3QD1tPErB41UGLxG9VoQd59jEWRudW6iPGm0IdMFb4oolU7Pu5G9S6pz&_nc_zt=23&_nc_ht=scontent.fepa9-2.fna&_nc_gid=O9vP9_M9x2tnr3tFTKmIxA&oh=00_AfLJhoaIncWU37KH87ydZkTL8irCEKCPkqXfUrN6lSeSeA&oe=682EEF75' alt='Logo del grupo' className='logo' />
+        <img src={Logo} alt='Logo del grupo' className='logo'  />
       </div>
       <nav className='nav-menu'>
         <ul>
