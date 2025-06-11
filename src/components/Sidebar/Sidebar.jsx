aa51c019a3e489931a9f523cadc0a318fee1648f
Catriel <catriellescobar@gmail.com>
2025-06-11 20:46:54 -0300

    fix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import './Sidebar.css';
-import Logo  from '../../assets/BreakingCode.png';
-
+import Logo from '../../assets/BreakingCode.png';
+// comentario
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,21 +14,47 @@ const Sidebar = () => {
       <button className='menu-toggle' onClick={toggleMenu}>
         {isOpen ? (
           // Ícono de "X"
-          <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="6" y1="6" x2="22" y2="22" stroke="#fff" strokeWidth="3" strokeLinecap="round"/>
-            <line x1="22" y1="6" x2="6" y2="22" stroke="#fff" strokeWidth="3" strokeLinecap="round"/>
+          <svg
+            width='28'
+            height='28'
+            viewBox='0 0 28 28'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'>
+            <line
+              x1='6'
+              y1='6'
+              x2='22'
+              y2='22'
+              stroke='#fff'
+              strokeWidth='3'
+              strokeLinecap='round'
+            />
+            <line
+              x1='22'
+              y1='6'
+              x2='6'
+              y2='22'
+              stroke='#fff'
+              strokeWidth='3'
+              strokeLinecap='round'
+            />
           </svg>
         ) : (
           // Ícono de menú
-          <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect y="5" width="28" height="3" rx="1.5" fill="#fff"/>
-            <rect y="12.5" width="28" height="3" rx="1.5" fill="#fff"/>
-            <rect y="20" width="28" height="3" rx="1.5" fill="#fff"/>
+          <svg
+            width='28'
+            height='28'
+            viewBox='0 0 28 28'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'>
+            <rect y='5' width='28' height='3' rx='1.5' fill='#fff' />
+            <rect y='12.5' width='28' height='3' rx='1.5' fill='#fff' />
+            <rect y='20' width='28' height='3' rx='1.5' fill='#fff' />
           </svg>
         )}
       </button>
       <div className='logo-container'>
-        <img src={Logo} alt='Logo del grupo' className='logo'  />
+        <img src={Logo} alt='Logo del grupo' className='logo' />
       </div>
       <nav className='nav-menu'>
         <ul>
